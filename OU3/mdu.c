a86980afb4036813d05cd8e73df803383cac6bb2
Filip Kanon <71726751+TacoSombrero@users.noreply.github.com>
2023-10-11 13:45:12 +0200

    single threaded kinda works

--- OU3/mdu.c
+++ OU3/mdu.c
@@ -21,7 +21,7 @@ int get_options(int argc, char *argv[]);
 int find_files(void *items);
 void open_dir(char *s, toThread *toThread);
 int get_size_of_file(char *name_of_file);
-char *get_new_path(char *current_dir, char *name, bool is_directory);
+void get_new_path(char *current_dir, char *name, bool is_directory, char *new_path);
 char *check_string(char *s);
 
 int main(int argc, char *argv[]){
@@ -37,17 +37,27 @@ int main(int argc, char *argv[]){
 	toThread->size = 0;
 	toThread->number_of_threads = number_of_threads;
 	
+	
 	bool use_cwd = true;
 	for (; optind < argc; optind++)
 	{
 		use_cwd = false;
+		/*int len = strlen(argv[optind]);
+		if (argv[optind][len - 1] == '\n'){
+			argv[optind][len - 1] = '\0';
+		}*/
+		int len = strlen(argv[optind]);
+		char s[len];
+		strcpy(s, argv[optind]);
 		open_dir(argv[optind], toThread);
-		printf("%d\t%s\n", toThread->size, argv[optind]);
+		toThread->size += get_size_of_file(argv[optind]);
+		printf("%d\t%s\n", toThread->size, s);
 	}
 
 	if(use_cwd){
 		open_dir("./", toThread);
-		printf("%d\t%s\n", toThread->size, "./");
+		toThread->size += get_size_of_file(".");
+		printf("%d\t%s\n", toThread->size, ".");
 	}
 
 	for(int i = 0; i < toThread->j; i++){
@@ -92,6 +102,8 @@ void open_dir(char *str, toThread *toThread){
 	DIR *dir = malloc(sizeof(DIR*));
 	dir = opendir(str);
 	toThread->ds[toThread->j] = dir;
+	//char *new_file_path;
+	//char *new_dir_path;
 	if(dir != NULL){
 
 		int len = strlen(str);
@@ -102,53 +114,26 @@ void open_dir(char *str, toThread *toThread){
 		int i = 0;
 		struct dirent *pDirent;
 		
+		
 		for(i = 0; (pDirent = readdir(dir)) != NULL; i++){
 			if((strncmp(pDirent->d_name, ".", 2) == 0) || (strncmp(pDirent->d_name, "..", 3) == 0)){
 				continue;
 			}
+			int len = strlen(pDirent->d_name) + strlen(str) + 2;
+			char *new_path = malloc(len * sizeof(char));
 			if(pDirent->d_type == 4){
 				
-				char *new_dir_path = get_new_path(str, pDirent->d_name, true);
-				toThread->size += get_size_of_file(new_dir_path);
-				//fprintf(stderr,"%s is dir\n", pDirent->d_name);
-				open_dir(new_dir_path, toThread);
+				get_new_path(str, pDirent->d_name, true, new_path);
+				toThread->size += get_size_of_file(new_path);
+				open_dir(new_path, toThread);
 			}else{
-				char *new_file_path = get_new_path(str, pDirent->d_name, false);
-				toThread->size += get_size_of_file(new_file_path);
-				//fprintf(stderr, "%s is file\n", pDirent->d_name);
+				get_new_path(str, pDirent->d_name, false, new_path);
+				toThread->size += get_size_of_file(new_path);
 			}
-		}
+		}	
 	}else{
-		//fprintf(stderr, "DIR == NULL\n");
 		toThread->size = get_size_of_file(str);
 	}
-	// s is a file
-	// Get size of the file.
-}
-
-char *check_string(char *s){
-	int len = strlen(s);
-	char *str = malloc(sizeof(len+1));
-	int i = 0;
-	if (s[len - 1] == '\n'){
-		s[len - 1] = '\0';
-	}
-	for(i = 0;s[i] != '\0'; i++){
-		str[i] = s[i];
-	}
-
-	if(s[len-1] != '/'){
-
-		int i = 0;
-		for(i = 0; i < len-1; i++){
-			str[i] = s[i];
-		}
-		str[i] = '/';
-		str[i+1] = '\0';
-
-		return str;
-	}
-	return s;
 }
 
 /**
@@ -157,22 +142,10 @@ char *check_string(char *s){
  * @return: The size of the file.
 */
 int get_size_of_file(char *name_of_file){
-/*
-	FILE *fp = fopen(name_of_file, "r");
-	if(fp == NULL){
-		DIR *dir = opendir(name_of_file);
-		if(dir == NULL){
-			fprintf(stderr, "Could not open file: %s\n", name_of_file);
-			return 0;
-		}
-		closedir(dir);
-	}
-	fclose(fp);*/
 
 	struct stat info;
 	if(lstat(name_of_file, &info)){
 		perror(name_of_file);
-		//exit(EXIT_FAILURE);
 		return 0;
 	}
 
@@ -181,29 +154,11 @@ int get_size_of_file(char *name_of_file){
 	return size;
 }
 
-char *get_new_path(char *current_dir, char *name, bool is_directory){
-
-	
-	int x = strlen(name);
-	int y = strlen(current_dir);
-
-	char *new_dir = malloc(x+y+2);
-	int i = 0;
-	while(current_dir[i] != '\0'){
-		new_dir[i] = current_dir[i];
-		i++;
-	}
-	int j = 0;
-	while (name[j] != '\0'){
-		new_dir[i] = name[j];
-		i++;
-		j++;
-	}
+void get_new_path(char *current_dir, char *name, bool is_directory, char *new_path){
+	strcpy(new_path, current_dir);
+	strcat(new_path, name);
 
 	if(is_directory){
-		new_dir[i] = '/';
+		strcat(new_path, "/");
 	}
-
-	return new_dir;
-	
 }
